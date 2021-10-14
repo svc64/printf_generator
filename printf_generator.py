1cdec96d7e655c5106f2973c6754300da112c040
svc64 <>
2021-10-14 13:21:24 +0300

    shorter code

--- printf_generator.py
+++ printf_generator.py
@@ -5,8 +5,7 @@ if len(sys.argv) != 2:
     print(f"usage: {sys.argv[0]} <text_file>")
 else:
     text_file = open(sys.argv[1], 'r')
-    text_lines = text_file.readlines()
-    text_file.close()
-    for line in text_lines:
+    for line in text_file.readlines():
         printf_call = "printf(\"" + "%c" * len(line) + "\"" + ''.join([", " + str(ord(c)) for c in line]) + ");"
         print(printf_call)
+    text_file.close()
